aa49220be9044b377d6dd15b6de1d7c213d6377f
André <AndreR@ppedv.de>
2023-08-14 14:00:52 +0200

    TDD Opening Hourse

---
+++ TddBank/TddBank.Tests/OpeningHoursTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TddBank.Tests
+{
+    public class OpeningHoursTests
+    {
+        [Theory]
+        [InlineData(2023, 8, 14, 10, 30, true)]//mo
+        [InlineData(2023, 8, 14, 10, 29, false)]//mo
+        [InlineData(2023, 8, 14, 10, 31, true)] //mo
+        [InlineData(2023, 8, 14, 18, 59, true)] //mo
+        [InlineData(2023, 8, 14, 19, 00, false)] //mo
+        [InlineData(2023, 8, 19, 13, 0, true)] //sa
+        [InlineData(2023, 8, 19, 16, 0, false)] //sa
+        [InlineData(2023, 8, 20, 20, 0, false)] //so
+        public void OpeningHours_IsOpen(int y, int M, int d, int h, int m, bool result)
+        {
+            var dt = new DateTime(y, M, d, h, m, 0);
+            var oh = new OpeningHours();
+
+            Assert.Equal(result, oh.IsOpen(dt));
+        }
+    }
+}
---
+++ TddBank/TddBank/OpeningHours.cs
@@ -0,0 +1,40 @@
+﻿namespace TddBank
+{
+    public class OpeningHours
+    {
+        private Dictionary<DayOfWeek, List<(TimeSpan start, TimeSpan end)>> schedule;
+
+        public OpeningHours()
+        {
+            schedule = new Dictionary<DayOfWeek, List<(TimeSpan, TimeSpan)>>
+            {
+                [DayOfWeek.Monday] = new List<(TimeSpan, TimeSpan)>
+            {
+                (new TimeSpan(10, 30, 0), new TimeSpan(18, 59, 0))
+            },
+                [DayOfWeek.Saturday] = new List<(TimeSpan, TimeSpan)>
+            {
+                (new TimeSpan(13, 0, 0), new TimeSpan(15, 59, 0))
+            },
+                // Define the opening hours for other days as well
+            };
+        }
+
+        public bool IsOpen(DateTime dateTime)
+        {
+            if (schedule.TryGetValue(dateTime.DayOfWeek, out List<(TimeSpan, TimeSpan)> openingHours))
+            {
+                foreach (var (start, end) in openingHours)
+                {
+                    var timeOfDay = dateTime.TimeOfDay;
+                    if (timeOfDay >= start && timeOfDay <= end)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
