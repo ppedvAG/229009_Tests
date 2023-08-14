83ed45ae35c3a9a25bf327af2765d43d38d54d2a
André <AndreR@ppedv.de>
2023-08-14 12:29:20 +0200

    TddBank test erstellt

---
+++ TddBank/TddBank.Tests/BankAccountTests.cs
@@ -0,0 +1,65 @@
+namespace TddBank.Tests
+{
+    public class BankAccountTests
+    {
+        [Fact]
+        public void New_account_should_have_zero_as_balance()
+        {
+            var ba = new BankAccount();
+
+            Assert.Equal(0, ba.Balance);
+        }
+
+        [Fact]
+        public void Deposit_should_add_to_Balance()
+        {
+            var ba = new BankAccount();
+
+            ba.Deposit(12m);
+            ba.Deposit(8m);
+
+            Assert.Equal(20m, ba.Balance);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        public void Deposit_a_negative_or_zero_value_should_throw_ArgumentException(decimal amount)
+        {
+            var ba = new BankAccount();
+
+            Assert.Throws<ArgumentException>(() => ba.Deposit(amount));
+        }
+
+        [Fact]
+        public void Withdraw_should_substract_from_Balance()
+        {
+            var ba = new BankAccount();
+            ba.Deposit(20m);
+
+            ba.Withdraw(8m);
+            ba.Withdraw(2m);
+
+            Assert.Equal(10m, ba.Balance);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        public void Withdraw_a_negative_or_zero_value_should_throw_ArgumentException(decimal amount)
+        {
+            var ba = new BankAccount();
+
+            Assert.Throws<ArgumentException>(() => ba.Withdraw(amount));
+        }
+
+        [Fact]
+        public void Withdraw_more_than_balance_throws_InvaildOperationEx()
+        {
+            var ba = new BankAccount();
+            ba.Deposit(20m);
+
+            Assert.Throws<InvalidOperationException>(() => ba.Withdraw(21));
+        }
+    }
+}
