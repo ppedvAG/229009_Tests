7e2a05ac996aa5731e81137d49f1ec76811e3e04
André <AndreR@ppedv.de>
2023-08-15 11:01:06 +0200

    CRUD Tests für Books

--- BooksManager/BooksManager.Tests/BooksManagerContextTests.cs
+++ BooksManager/BooksManager.Tests/BooksManagerContextTests.cs
@@ -53,5 +53,60 @@ namespace BooksManager.Tests
                 Assert.Equal(loaded.Title, book.Title);
             }
         }
+
+        [Fact]
+        [Trait("", "Integration")]
+        public void Can_udate_Book()
+        {
+            var book = new Book() { Title = $"Testing is Fun_{Guid.NewGuid()}", Description = "Fun with Unittests", Pages = 100 };
+            var newTitle = $"Testing is pain_{Guid.NewGuid()}";
+            using (var con = new BooksManagerContext(conString))
+            {
+                con.Database.EnsureCreated();
+                con.Books.Add(book);
+                con.SaveChanges();
+            }
+
+            using (var con = new BooksManagerContext(conString))
+            {
+                var loaded = con.Books.Find(book.Id);
+                loaded.Title = newTitle;
+                var rows = con.SaveChanges();
+                Assert.Equal(1, rows);
+            }
+
+            using (var con = new BooksManagerContext(conString))
+            {
+                var loaded = con.Books.Find(book.Id);
+                Assert.Equal(loaded.Title, newTitle);
+            }
+        }
+
+        [Fact]
+        [Trait("", "Integration")]
+        public void Can_delete_Book()
+        {
+            var book = new Book() { Title = $"Testing is Fun_{Guid.NewGuid()}", Description = "Fun with Unittests", Pages = 100 };
+            using (var con = new BooksManagerContext(conString))
+            {
+                con.Database.EnsureCreated();
+                con.Books.Add(book);
+                con.SaveChanges();
+            }
+
+            using (var con = new BooksManagerContext(conString))
+            {
+                var loaded = con.Books.Find(book.Id);
+                con.Remove(loaded);
+                var rows = con.SaveChanges();
+                Assert.Equal(1, rows);
+            }
+
+            using (var con = new BooksManagerContext(conString))
+            {
+                var loaded = con.Books.Find(book.Id);
+                Assert.Null(loaded);
+            }
+        }
     }
 }
