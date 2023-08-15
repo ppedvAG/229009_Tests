904762045bf63b8e92511827d1546a7c456b9d51
André <AndreR@ppedv.de>
2023-08-15 14:22:52 +0200

    UI Fertig

--- BooksManager/BooksManager/MainWindow.xaml.cs
+++ BooksManager/BooksManager/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using BooksManager.Data;
 using BooksManager.Model;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,16 +21,40 @@ namespace BooksManager
     {
         readonly string conString = "Server=(localdb)\\mssqllocaldb;Database=BooksManager_tests;Trusted_Connection=true";
         IRepository repository;
+        ObservableCollection<Book> bookList = new ObservableCollection<Book>();
 
         public MainWindow()
         {
             InitializeComponent();
             repository = new EfCoreRepository(conString);
+            myGrid.ItemsSource = bookList;
         }
 
         private void LoadBooks(object sender, RoutedEventArgs e)
         {
-            myGrid.ItemsSource = repository.GetAll<Book>();
+            bookList.Clear();
+            repository.GetAll<Book>().ToList().ForEach(x => bookList.Add(x));
+        }
+
+        private void SaveAll(object sender, RoutedEventArgs e)
+        {
+            repository.SaveAll();
+        }
+
+        private void AddNewBook(object sender, RoutedEventArgs e)
+        {
+            var newB = new Book() { Title = "NUE" };
+            repository.Add(newB);
+            bookList.Add(newB);
+        }
+
+        private void DeleteBook(object sender, RoutedEventArgs e)
+        {
+            if( myGrid.SelectedItem is Book b)
+            {
+                repository.Delete(b);
+                bookList.Remove(b);
+            }
         }
     }
 }
