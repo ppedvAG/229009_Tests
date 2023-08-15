4675cf9faa9b1447e2d5a9aa85ce45fa2cdfdfdb
André <AndreR@ppedv.de>
2023-08-15 14:14:40 +0200

    EfCoreRepository

---
+++ BooksManager/BooksManager/Data/EfCoreRepository.cs
@@ -0,0 +1,44 @@
+﻿using BooksManager.Model;
+
+namespace BooksManager.Data
+{
+    internal class EfCoreRepository : IRepository
+    {
+        private readonly BooksManagerContext context;
+
+        public EfCoreRepository(string conString)
+        {
+            context = new BooksManagerContext(conString);
+        }
+
+        public void Add<T>(T entity) where T : class
+        {
+            context.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            context.Remove(entity);
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : class
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public T GetById<T>(int id) where T : class
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public void SaveAll()
+        {
+            context.SaveChanges();
+        }
+
+        public void Update<T>(T entity) where T : class
+        {
+            context.Update(entity);
+        }
+    }
+}
